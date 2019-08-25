ed5847bafbd062e4ae04fc5deb533eab476b2a9a
Assurance 1 <samsonendurance55@gmail.com>
2019-08-25 19:36:42 +0100

    Add files via upload

---
+++ endurance.py
@@ -0,0 +1,21 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> x= int(input("Enter x Number:"))
+Enter x Number:56
+>>> y = int(input("Enter y Number:"))
+Enter y Number:6
+>>> answer = x**y
+>>> print (answer)
+30840979456
+>>> print ("x raised to the power of y is:", answer)
+x raised to the power of y is: 30840979456
+>>> print (math.log(x))
+4.02535169073515
+>>> print (math.log(x, 2))
+5.807354922057605
+>>> print (math.log2(x))
+5.807354922057604
+>>> print (x^y)
+62
+>>> 
